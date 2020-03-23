97c0debe9f305eb6cb268e4b76ed26fe9b5652cc
diego-cruz <diegocruz2@outlook.com>
2020-03-23 12:38:43 -0300

    ultrafer v1

---
+++ js/calculadoraultrafer.js
@@ -0,0 +1,170 @@
+// Calculadora Ultrafer
+
+// Situação e peso
+
+
+  
+
+
+ 
+
+// pré-termo até 1kg
+
+if (situacao == "pretermo1kg" && (kg >= 0.5 && kg <= 1.1)) {
+    window.alert("1 gota 1x ao dia");   
+} else if (situacao == "pretermo1kg" && (kg >= 1.2 && kg <= 1.7)){
+     window.alert("2 gotas 1x ao dia");   
+} else if (situacao == "pretermo1kg" && (kg >= 1.8 && kg <= 2.3)){
+     window.alert("3 gotas 1x ao dia"); 
+} else if (situacao == "pretermo1kg" && (kg >= 2.4 && kg <= 2.9)){
+     window.alert("4 gotas 1x ao dia"); 
+} else if (situacao == "pretermo1kg" && (kg >= 3 && kg <= 3.6)){
+     window.alert("5 gotas 1x ao dia"); 
+} else if (situacao == "pretermo1kg" && (kg >= 3.7 && kg <= 4.2)){
+     window.alert("6 gotas 1x ao dia"); 
+} else if (situacao == "pretermo1kg" && (kg >= 4.3 && kg <= 4.8)){
+     window.alert("7 gotas 1x ao dia"); 
+} else if (situacao == "pretermo1kg" && (kg >= 4.9 && kg <= 5.4)){
+     window.alert("8 gotas 1x ao dia");     
+} else if (situacao == "pretermo1kg" && (kg >= 5.5 && kg <= 6.1)){
+     window.alert("9 gotas 1x ao dia");     
+} else if (situacao == "pretermo1kg" && (kg >= 6.2 && kg <= 6.7)){
+     window.alert("10 gotas 1x ao dia");     
+} else if (situacao == "pretermo1kg" && (kg >= 6.8 && kg <= 7.3)){
+     window.alert("11 gotas 1x ao dia");     
+} else if (situacao == "pretermo1kg" && (kg >= 7.4 && kg <= 7.9)){
+     window.alert("12 gotas 1x ao dia");     
+} else if (situacao == "pretermo1kg" && (kg >= 8 && kg <= 8.6)){
+     window.alert("13 gotas 1x ao dia");     
+} else if (situacao == "pretermo1kg" && (kg >= 8.7 && kg <= 9.2)){
+     window.alert("14 gotas 1x ao dia");     
+} else if (situacao == "pretermo1kg" && (kg >= 9.3 && kg <= 9.8)){
+     window.alert("15 gotas 1x ao dia");     
+} else if (situacao == "pretermo1kg" && (kg >= 9.9 && kg <= 10)){
+     window.alert("16 gotas 1x ao dia");     
+}
+
+
+
+// pré-termo entre 1kg-1,5kg
+
+if (situacao == "pretermo1.5kg" && (kg >= 1 && kg <= 1.4)) {
+    window.alert("1 gota 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 1.5 && kg <= 2.3)){
+     window.alert("2 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 2.4 && kg <= 3.1)){
+     window.alert("3 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 3.2 && kg <= 4)){
+     window.alert("4 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 4.1 && kg <= 4.8)){
+     window.alert("5 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 4.9 && kg <= 5.6)){
+     window.alert("6 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 5.7 && kg <= 6.4)){
+     window.alert("7 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 6.5 && kg <= 7.3)){
+     window.alert("8 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 7.4 && kg <= 8.1)){
+     window.alert("9 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 8.2 && kg <= 8.9)){
+     window.alert("10 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 9 && kg <= 9.8)){
+     window.alert("11 gotas 1x ao dia");   
+} else if (situacao == "pretermo1.5kg" && (kg >= 9.9 && kg <= 10)){
+     window.alert("12 gotas 1x ao dia");   
+} 
+
+
+
+
+// pré-termo entre 1,5kg-2,5kg
+
+if (situacao == "pretermo2.5kg" && (kg >= 1.5 && kg <= 2.2)) {
+    window.alert("1 gota 1x ao dia");   
+} else if (situacao == "pretermo2.5kg" && (kg >= 2.3 && kg <= 3.4)){
+     window.alert("2 gotas 1x ao dia");   
+} else if (situacao == "pretermo2.5kg" && (kg >= 3.5 && kg <= 4.7)){
+     window.alert("3 gotas 1x ao dia");   
+} else if (situacao == "pretermo2.5kg" && (kg >= 4.8 && kg <= 5.9)){
+     window.alert("4 gotas 1x ao dia");   
+} else if (situacao == "pretermo2.5kg" && (kg >= 6 && kg <= 7.2)){
+     window.alert("5 gotas 1x ao dia");   
+} else if (situacao == "pretermo2.5kg" && (kg >= 7.3 && kg <= 8.4)){
+     window.alert("6 gotas 1x ao dia");   
+} else if (situacao == "pretermo2.5kg" && (kg >= 8.5 && kg <= 9.7)){
+     window.alert("7 gotas 1x ao dia");   
+}  else if (situacao == "pretermo2.5kg" && (kg >= 9.8 && kg <= 10)){
+     window.alert("8 gotas 1x ao dia");   
+}
+
+
+
+// a termo com menos de 2,5kg
+
+if (situacao == "atermomenor2.5kg" && (kg >= 2 && kg <= 4.4)) {
+    window.alert("1 gota 1x ao dia");   
+} else if (situacao == "atermomenor2.5kg" && (kg >= 4.5 && kg <= 6.9)){
+     window.alert("2 gotas 1x ao dia");   
+} else if (situacao == "atermomenor2.5kg" && (kg >= 7 && kg <= 9.4)){
+     window.alert("3 gotas 1x ao dia");   
+} else if (situacao == "atermomenor2.5kg" && (kg >= 9.5 && kg <= 10.9)){
+     window.alert("4 gotas 1x ao dia");   
+}
+
+
+
+
+// deficiência de ferro
+
+if (situacao == "defdeferro" && (kg >= 5.1 && kg <= 5.7)) {
+    window.alert("5 gotas 1x ao dia");   
+} else if (situacao == "defdeferro" && (kg >= 5.8 && kg <= 6.7)){
+     window.alert("6 gotas 1x ao dia");   
+} else if (situacao == "defdeferro" && (kg >= 6.8 && kg <= 7.7)){
+     window.alert("7 gotas 1x ao dia");   
+} else if (situacao == "defdeferro" && (kg >= 7.8 && kg <= 8.7)){
+     window.alert("8 gotas 1x ao dia");   
+} else if (situacao == "defdeferro" && (kg >= 8.8 && kg <= 9.7)){
+     window.alert("9 gotas 1x ao dia");   
+} else if (situacao == "defdeferro" && (kg >= 9.8 && kg <= 10)){
+     window.alert("10 gotas 1x ao dia");   
+} else if (situacao == "defdeferro" && (kg >= 10.1 && kg <= 35)){
+     window.alert("10 gotas 1x ou 2x ao dia");   
+}
+
+
+
+
+// Anemia
+
+if (situacao == "anemia" && (kg >= 2.5 && kg <= 3.7)) {
+    window.alert("1 gota 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 3.8 && kg <= 5)){
+     window.alert("2 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 5.1 && kg <= 5.3)){
+     window.alert("10 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 5.4 && kg <= 5.8)){
+     window.alert("11 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 5.9 && kg <= 6.3)){
+     window.alert("12 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 6.4 && kg <= 6.8)){
+     window.alert("13 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 6.9 && kg <= 7.3)){
+     window.alert("14 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 7.4 && kg <= 7.8)){
+     window.alert("15 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 7.9 && kg <= 8.3)){
+     window.alert("16 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 8.4 && kg <= 8.8)){
+     window.alert("17 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 8.9 && kg <= 9.3)){
+     window.alert("18 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 9.4 && kg <= 9.8)){
+     window.alert("19 gotas 1x ao dia"); 
+} else if (situacao == "anemia" && kg == 9.9){
+     window.alert("20 gotas 1x ao dia");   
+} else if (situacao == "anemia" && (kg >= 10 && kg <= 35)){
+     window.alert("20 gotas 1x ou 2x ao dia");   
+} else if (situacao == "anemia" && kg > 35){
+     window.alert("40 gotas 1x ou 2x ao dia");   
+}
